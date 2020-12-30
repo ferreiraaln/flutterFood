e0850ccb1ee7779ea078763d6e5765b702bad60f
Allan <allan_f2008@hotmail.com>
2020-12-29 22:23:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import './screens/auth/login_page.dart';
+
+void main() => runApp(FlutterFoodApp());
+
+class FlutterFoodApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterFood',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(147, 70, 248, 1),
+        backgroundColor: Colors.white,
+        accentColor: Colors.black,
+        brightness: Brightness.light,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/screens/auth/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
